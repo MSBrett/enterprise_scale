4f75e69bd07b5e54cf1bda76572899fe72a93df4
MSBrett <MSBrett@users.noreply.github.com>
2021-07-28 12:25:53 -0700

    Modify Backend

--- backend.tf
+++ backend.tf
@@ -4,5 +4,6 @@ terraform {
     storage_account_name = "eslzmgstate01"
     container_name       = "tfstate"
     key                  = "test.terraform.tfstate"
+    subscription_id      = "1234567890"
   }
 }
